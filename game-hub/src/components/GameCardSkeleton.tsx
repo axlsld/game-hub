6928e5fd6f5820b8d63cd0ec0766be717197a266
alexis <johnalexissaldua06@gmail.com>
2026-06-05 13:51:25 +0800

    Refactor: remove duplicated styles

--- game-hub/src/components/GameCardSkeleton.tsx
+++ game-hub/src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root width={'300px'} borderRadius={10} overflow={'hidden'}>
+    <Card.Root >
       <Skeleton height={'200px'}/>
       <CardBody>
         <SkeletonText/>
